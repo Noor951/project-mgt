a7373edc42c797bd8f7f32d0440a6fac7a73c165
Noor Fatima <fatimakahloon951@gmail.com>
2026-05-10 22:34:24 +0500

    fix database connection

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -3,7 +3,8 @@ import { PrismaClient } from "@prisma/client";
 import { Pool } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 
-const connectionString = process.env.DATABASE_URL;
+// configs/prisma.js
+const connectionString = "postgresql://neondb_owner:npg_LYZQBg7pPH6F@ep-weathered-feather-am1ddnwf-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require";
 
 if (!connectionString) {
   throw new Error("❌ DATABASE_URL is not set. Check your .env file.");
